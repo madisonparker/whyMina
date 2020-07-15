b7c99ebb4fc4f9bd10504b33d2a68bd135dca5d7
Madison <madison@parkerworld.com>
2020-07-15 14:26:28 -0400

    Initial Commit

---
+++ whyMina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whyMina
+//
+//  Created by Apple on 7/15/20.
+//  Copyright © 2020 Madison Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
